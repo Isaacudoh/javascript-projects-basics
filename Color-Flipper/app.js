52c3893c2f7bb054ce35eee5ce03acfeb7a5a1f3
nsikakudoh <nsikak@sparknerds.io>
2023-03-18 09:49:25 -0600

    add event listener

--- Color-Flipper/app.js
+++ Color-Flipper/app.js
@@ -1,1 +1,10 @@
-const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+let colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+
+let btn = document.getElementById("btn");
+let color = document.querySelector("color");
+
+btn.addEventListener("click", function () {
+  //get random number between 0 and 3
+    let randomNumber = 2;
+
+});
