02bd27654509b47c95165707933829c0d4d7f87d
nsikakudoh <nsikak@sparknerds.io>
2023-03-23 13:31:10 -0600

    complete color fliper

--- Color-Flipper/app.js
+++ Color-Flipper/app.js
@@ -1,11 +1,16 @@
 let colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
 
 let btn = document.getElementById("btn");
-let color = document.querySelector("color");
+let color = document.getElementsByClassName("color");
 
 btn.addEventListener("click", function () {
   //get random number between 0 and 3
-  let randomNumber = 2;
+  let randomNumber = getRandomNumber;
+  console.log(randomNumber);
   document.body.style.backgroundColor = colors[randomNumber];
   color.textContent = colors[randomNumber];
 });
+
+function getRandomNumber() {
+  return Math.floor(Math.random() * colors.length);
+}
